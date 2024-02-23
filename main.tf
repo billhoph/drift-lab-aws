544ccb9014ae81c91396e8c1d70c7219143128aa
hojohn269@msn.com <hojohn269@msn.com>
2024-02-24 00:13:46 +0800

    revert the change

--- main.tf
+++ main.tf
@@ -10,27 +10,17 @@ terraform {
 }
 
 provider "aws" {
-  region  = "ap-southeast-1"
+  region  = "us-west-2"
   profile = "default"
 }
 
 resource "aws_instance" "tf_demo_vm" {
-  ami           = "ami-07a6e3b1c102cdba8"
+  ami           = "ami-008fe2fc65df48dac"
   instance_type = "t2.micro"
 
   tags = {
     Name      = "tfDemoVM01"
     yor_name  = "tf_demo_vm"
-    yor_trace = "0ff69fe9-9823-46ee-a1ad-be11a8095c47"
   }
-  associate_public_ip_address = true
-  availability_zone = "ap-southeast-1b"
-  metadata_options = {"http_endpoint": "enabled", "http_put_response_hop_limit": "2", "http_tokens": "required"}
-  monitoring = false
-  private_ip = "172.31.20.192"
-  root_block_device = {"delete_on_termination": true, "encrypted": false, "iops": "3000", "volume_size": "8", "volume_type": "gp3"}
-  security_groups = ["default"]
-  subnet_id = "subnet-0f4f2002e0c52be9c"
-  vpc_security_group_ids = ["sg-07613ed1bd94fbb1e"]
 }
 
